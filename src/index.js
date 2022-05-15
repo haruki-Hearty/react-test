95b3c55e6a994a06d58943aaff03320152505eb1
haruki <79205378+haruki-Hearty@users.noreply.github.com>
2022-05-15 17:14:38 +0900

    テンプレート文字列 追加

--- src/index.js
+++ src/index.js
@@ -42,13 +42,13 @@
 /**
  * テンプレート文字列
  */
-const name = "haruki";
-const age = 35;
+// const name = "haruki";
+// const age = 35;
 
-//従来の方法 結合の度に + が必要
-const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
-console.log(message1);
+// //従来の方法 結合の度に + が必要
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+// console.log(message1);
 
-//テンプレート文字列を用いた方法
-const message2 = `私の名前は${name}です。年齢は${age}です。`;
-console.log(message2);
+// //テンプレート文字列を用いた方法
+// const message2 = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
